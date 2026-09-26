e76164aa04e6fcdd06b44a0a82251f0cdebabf43
suyashitiwari4 <suyashitiwari4@gmail.com>
2026-09-26 12:39:24 +0530

    [643] [Time Beats: 100.00%] [Memory Beats: 27.25%] - LeetPush

---
+++ 643-Maximum-Average-Subarray-I.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double max_sum=0;
+        double curr_sum=0;
+        for(int i=0;i<k;i++){
+            curr_sum+=nums[i];
+            
+        }
+        max_sum=curr_sum;
+        for(int i=k; i<nums.size();i++){
+            curr_sum+=nums[i]-nums[i-k];
+            max_sum=max(max_sum,curr_sum);
+        }
+        return max_sum/k;
+    }
+};
